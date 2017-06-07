26a96bc7d36fa402844704dff69c0a17e3126989
Yan <yan.tong@univ.ox.ac.uk>
2017-06-07 11:14:31 +0100

    made it compatible to tySprial.m Tests needed.

--- make_blochSim.m
+++ make_blochSim.m
@@ -1,10 +1,16 @@
-function [mxss1_mt,myss1_mt,mzss1_mt] = make_blochSim(rf,sens,g,tp,df,dp,Positions)
-posVecX = Positions(1,:,1,1);
-posVecY = Positions(2,1,:,1);
-posVecZ = Positions(3,1,1,:);
+function [mxss1_mt,myss1_mt,mzss1_mt] = make_blochSim(rf,b1,g,dt,Positions)
+posVecX = squeeze(Positions(1,:,1,1));
+posVecY = squeeze(Positions(2,1,:,1));
+posVecZ = squeeze(Positions(3,1,1,:));
+[X,Y,Z] = meshgrid(posVecX,posVecY,posVecZ);
+dp = [X(:) Y(:) Z(:)];
+df = zeros(size(dp,1),1);
 mode = 0;
-
-[mxss1_mt,myss1_mt,mzss1_mt] =  blochSim_mex_multiThread(rf,sens,g,tp,df,dp,mode);
+sens = b1(:);
+nRF = size(rf,1);
+nG = size(g,1);
+rfFull = [zeros(nG-nRF,1); rf];
+[mxss1_mt,myss1_mt,mzss1_mt] =  blochSim_mex_multiThread(rfFull,sens,g,dt,df,dp,mode);
 
 mxss1_mt = reshape(mxss1_mt,size(X));
 myss1_mt = reshape(myss1_mt,size(X));
